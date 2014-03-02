bc82b2ce4d941a4bc7d348e1c88ca85042d1ff3c
openautonomy <openautonomy@7bfa7221-89b1-4271-8c77-631a4289ab44>
2014-03-02 03:32:29 +0000

    Added function call processing and top-level function calls to the symbol table output -note that uses of "new" count as function calls, in this context, since the goal is dead code elimintation and that operator makes a specific constructor live

---
+++ trunk/tools/PhpCompiler/symbol_table/CallGlobalWalker.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ Copyright (c) 2014 Open Autonomy Inc.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+require_once('ITreeWalker.php');
+require_once('Symbol_GlobalCall.php');
+
+
+// Author:  Jeff Disher (Open Autonomy Inc.)
+// An implementation of the tree walker which extracts information regarding a call to a top-level global function.
+class OA_CallGlobalWalker implements OA_ITreeWalker
+{
+	const kIdentifier = 'IDENTIFIER';
+	
+	
+	private $functionNameToken;
+	
+	
+	// Creates an empty representation of the receiver.
+	public function __construct()
+	{
+		$this->functionNameToken = null;
+	}
+	
+	// OA_ITreeWalker.
+	public function preVisitTree($tree)
+	{
+		// Since we need to dig through the P_IDENTIFIER, just continue visiting until we have found our function name
+		//  token.
+		return (null === $this->functionNameToken);
+	}
+	
+	// OA_ITreeWalker.
+	public function postVisitTree($tree)
+	{
+		// Do nothing on exit.
+	}
+	
+	// OA_ITreeWalker.
+	public function visitLeaf($leaf)
+	{
+		// We are interested in the first identifier call we see, but nothing else.
+		//  (looking for "P_IDENTIFIER P_RARG_BRACKETS" but the IDENTIFIER underneath P_IDENTIFIER)
+		if (OA_CallGlobalWalker::kIdentifier === $leaf->getName())
+		{
+			assert(null === $this->functionNameToken);
+			$this->functionNameToken = $leaf;
+		}
+	}
+	
+	public function getFunctionCallObject()
+	{
+		return new OA_Symbol_GlobalCall($this->functionNameToken);
+	}
+}
+
+?>
---
+++ trunk/tools/PhpCompiler/symbol_table/IFunctionCall.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ Copyright (c) 2014 Open Autonomy Inc.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
+
+// Author:  Jeff Disher (Open Autonomy Inc.)
+// Function calls need to be handled via a consistent external API so we will use this interface to avoid the details
+//  of each kind of call.
+interface OA_IFunctionCall
+{
+	public function getDescription();
+}
+
+
+?>
